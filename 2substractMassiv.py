8286f3b15dcf949c6e58980510064c6561f7e830
Anton <43184533+motobeerx@users.noreply.github.com>
2019-08-26 15:47:02 +0300

    Update 2substractMassiv.py

--- 2substractMassiv.py
+++ 2substractMassiv.py
@@ -1,5 +1,7 @@
+import collections
 a, b =  ["Anton","Alex", "Dima",'Anton', "Kate", "Galina", "Ivan",'Marie','Denis', 'Anton',"Dima", "Doma", 'dsa', 'Galina', 'Kate', 'Kate', 'Kate', 'Anton','Anton'], ["Dima", "Ivan", "Kate",'Anton']
 print('\nOriginal List:', *a, '\n')
 print('Substracted List:', *b, '\n\nResult: ', sep=' ', end='')
-[print(a[i], end=' ') for i in range(len(a)) if a[i] not in b]
+d= collections.deque(a)
+[print(d[i], end=' ') for i in range(len(d)) if d[i] not in b]
 print('\n')
