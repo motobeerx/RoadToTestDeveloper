5f2ee50ed11e3a16a7c65e7426b0341718edd8d0
Anton <43184533+motobeerx@users.noreply.github.com>
2019-08-27 00:17:21 +0300

    Update 3randomTestCases.py

--- 3randomTestCases.py
+++ 3randomTestCases.py
@@ -5,15 +5,14 @@ import random
 def randomeTestCases():
     while True:
         try:
-            #print(r"Insert file's path (for example C:/User/Documents/test.txt): ", end='')
-            #path = input()
-            path = r'C:\Users\Nagibator\Downloads\1.txt'
+            print(r"Insert file's path (for example C:/User/Documents/test.txt): ", end='')
+            path = input()
             print(r"Insert number of test cases: ", end='')
             n = int(input())
             line_list = []
             random_cases_number = [0]
             assert os.path.exists(path)
-            with open(path, 'rw') as f:
+            with open(path, 'r') as f:
                 f_content = f.readline()
                 while f_content != '':
                     line_list.append(f_content)
@@ -26,6 +25,7 @@ def randomeTestCases():
                         random_cases_number.pop()
                         i -= 1
                     i += 1
+            with open(path, 'w') as f:
                 [f.write(line_list[i]) for i in range(len(line_list)) if i not in random_cases_number[1:]]
             res_path = os.path.split(path)
             res_path = os.path.join(res_path[0] + r'\res_' + res_path[1])
@@ -50,5 +50,4 @@ def randomeTestCases():
                 return
 
 
-#C:\Users\Nagibator\Downloads\1.txt
 print(randomeTestCases())
