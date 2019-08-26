1d01eccee3b8084d026bb7b1e9de2fc4dc67eece
Anton <43184533+motobeerx@users.noreply.github.com>
2019-08-26 14:58:13 +0300

    Update 2substractMassiv.py

--- 2substractMassiv.py
+++ 2substractMassiv.py
@@ -1,5 +1,5 @@
 a, b =  ["Anton","Alex", "Dima",'Anton', "Kate", "Galina", "Ivan",'Marie','Denis', 'Anton',"Dima", "Doma", 'dsa', 'Galina', 'Kate', 'Kate', 'Kate', 'Anton','Anton'], ["Dima", "Ivan", "Kate",'Anton']
 print('\nOriginal List:', *a, '\n')
 print('Substracted List:', *b, '\n\nResult: ', sep=' ', end='')
-[print(x, end=' ') for x in a if x not in b]
+[print(a[i], end=' ') for i in range(len(a)) if a[i] not in b]
 print('\n')
