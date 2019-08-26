305895b8daf5d50b803b5610cde7023ba541b42f
Anton <43184533+motobeerx@users.noreply.github.com>
2019-08-27 00:12:31 +0300

    Update 3randomTestCases.py
    error: the number of test cases is always taken as str type!

--- 3randomTestCases.py
+++ 3randomTestCases.py
@@ -5,45 +5,50 @@ import random
 def randomeTestCases():
     while True:
         try:
-            print(r"Insert file's path (for example C:/User/Documents/test.txt): ", end='')
-            path = input()
-            print(r"Insert file numbers: ", end='')
+            #print(r"Insert file's path (for example C:/User/Documents/test.txt): ", end='')
+            #path = input()
+            path = r'C:\Users\Nagibator\Downloads\1.txt'
+            print(r"Insert number of test cases: ", end='')
             n = int(input())
             line_list = []
             random_cases_number = [0]
-            if not os.path.exists(path):
-                raise FileExistsError
-
-            with open(path) as f:
+            assert os.path.exists(path)
+            with open(path, 'rw') as f:
                 f_content = f.readline()
                 while f_content != '':
                     line_list.append(f_content)
                     f_content = f.readline()
-            f.close()
-
-            i = 2
-            random_cases_number.append(random.randint(1, len(line_list) - 1))  # think it is not nesesary and i =1
-
-            while i < n + 1:
-                random_cases_number.append(random.randint(1, len(line_list) - 1))
-                if random_cases_number[i] in random_cases_number[:i]:
-                    random_cases_number.pop()
-                    i -= 1
-                i += 1
-
+                assert n <= len(line_list)
+                i = 1
+                while i < n + 1:
+                    random_cases_number.append(random.randint(1, len(line_list) - 1))
+                    if random_cases_number[i] in random_cases_number[:i]:
+                        random_cases_number.pop()
+                        i -= 1
+                    i += 1
+                [f.write(line_list[i]) for i in range(len(line_list)) if i not in random_cases_number[1:]]
             res_path = os.path.split(path)
             res_path = os.path.join(res_path[0] + r'\res_' + res_path[1])
-
             with open(res_path, 'w') as f:
                 for x in random_cases_number:
                     f.write(line_list[x])
-            f.close()
-        except FileExistsError:
-            print('\nThe given path "' + path + '" does not exist.')
+            return res_path
+        except AssertionError:
+            if not os.path.exists(path):
+                print('\nThe given path "' + path + '" does not exist.')
+            if n > len(line_list):
+                print(f'Opps.. The inserted number of test cases bigger than current number in the file - {len(line_list)}')
+            print('Would you like to try one more time? Y/N: ', end='')
+            contin = input()
+            if contin == 'N' or contin == 'n':
+                return
+        except ValueError:
+            print("Test cases number must be integer type !")
             print('Would you like to try one more time? Y/N: ', end='')
             contin = input()
             if contin == 'N' or contin == 'n':
                 return
 
-#/home/anton/Downloads/1.txt
-randomeTestCases()
+
+#C:\Users\Nagibator\Downloads\1.txt
+print(randomeTestCases())
