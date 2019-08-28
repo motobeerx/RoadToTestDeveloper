4aedf6fea73aae77f38e358823b8eab5b11f0f09
Anton <43184533+motobeerx@users.noreply.github.com>
2019-08-28 18:44:09 +0300

    Update 1freshFileSearch.py

--- 1freshFileSearch.py
+++ 1freshFileSearch.py
@@ -5,11 +5,12 @@ from datetime import datetime
 def freashFileSearch():
     while True:
         try:
-            print(r"Enter folder's path (for example C:/User/Documents): ", end='')
+            print(r"Enter folder's path (for example C:\User\Documents): ", end='')
             path = input()
             print(r"Enter file extension (for example .txt): ", end='')
             ext = input()
-            assert os.path.isdir(path)
+            if not os.path.isdir(path):
+                raise FileExistsError
             files = os.listdir(path)
             if ext[0] != '.':
                 ext = '.' + ext
@@ -21,11 +22,14 @@ def freashFileSearch():
             for i in range(len(files)):
                 print(files[i][0] + '  ||  ' + files[i][1])
             return
+        except FileExistsError:
+            print('\nThe given directory "' + path + '" does not exist.')
+            print('Would you like to try one more time ? Y/N: ', end='')
+            cont_descis = input()
+            if cont_descis == 'N' or cont_descis == 'n':
+                return
         except AssertionError:
-            if not os.path.isdir(path):
-                print('\nThe given directory "' + path + '" does not exist.')
-            if len(files) == 0:
-                print('\nNo " *' + ext + ' " file has been found in the "' + path + '"')
+            print('\nNo " *' + ext + ' " file has been found in the "' + path + '"')
             print('Would you like to try one more time ? Y/N: ', end='')
             cont_descis = input()
             if cont_descis == 'N' or cont_descis == 'n':
